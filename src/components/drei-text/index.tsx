1e874eee17c1e9ae464dcaba0ae6d58924aeaea1
grant <grant@artblocks.io>
2024-02-04 12:32:19 -0600

    fixing errors

--- src/components/drei-text/index.tsx
+++ src/components/drei-text/index.tsx
@@ -1,12 +1,7 @@
-import {
-  MeshProps,
-  ReactThreeFiber,
-  useFrame,
-  useThree,
-} from '@react-three/fiber'
-import { useRef, useState } from 'react'
+import { MeshProps } from '@react-three/fiber'
+import { useRef } from 'react'
 import { Text } from '@react-three/drei'
-import THREE, { Color, Mesh } from 'three'
+import { Mesh } from 'three'
 
 export interface TextProps extends MeshProps {
   text: string
@@ -14,21 +9,8 @@ export interface TextProps extends MeshProps {
 }
 
 export default function FunHeaderText(props: TextProps) {
-  const t = useThree()
   const ref = useRef<Mesh>()
   // Create a new geometry
-  useFrame((state, delta) => {
-    if (!ref.current) return
-    // let orig_z = (props.position as number[])[2];
-    // ref.current.position.z = THREE.MathUtils.damp(ref.current.position.z, Math.max(orig_z, orig_z + data.delta * 50), 4, delta)
-    if (t.size.width >= 700) {
-      ref.current.rotation.x = -t.mouse.y / 3
-      ref.current.rotation.y = t.mouse.x / 5
-    } else {
-      ref.current.rotation.x = 0
-      ref.current.rotation.y = 0
-    }
-  })
 
   return (
     <Text
